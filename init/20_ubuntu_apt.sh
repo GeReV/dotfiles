77cf185e41277ff1abaf4c3af0d7197cc69a0107
Amir Grozki <amirgrozki@gmail.com>
2016-04-09 11:49:15 +0300

    Removed redundant test for file from old version of this dotfiles repo.

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -1,9 +1,6 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
-# If the old files isn't removed, the duplicate APT alias will break sudo!
-sudoers_old="/etc/sudoers.d/sudoers-cowboy"; [[ -e "$sudoers_old" ]] && sudo rm "$sudoers_old"
-
 # Installing this sudoers file makes life easier.
 sudoers_file="sudoers-dotfiles"
 sudoers_src=$DOTFILES/conf/ubuntu/$sudoers_file
