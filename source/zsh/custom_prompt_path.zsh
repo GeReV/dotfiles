35f04ecd4bd03053d8f9993aa52373fa02442ac2
Amir Grozki <EMAIL>
2022-03-13 00:23:53 +0200

    Another fix to prezto directory.

--- source/zsh/custom_prompt_path.zsh
+++ source/zsh/custom_prompt_path.zsh
@@ -1,4 +1,4 @@
 #Load themes from yadr and from user's custom prompts (themes) in ~/.zsh.prompts
 autoload promptinit
-fpath=($DOTFILES/source/zsh/prezto-themes $DOTFILES/vendor/prezto/modules/prompt/functions $HOME/.zsh.prompts $fpath)
+fpath=($DOTFILES/source/zsh/prezto-themes ~/.zprezto/modules/prompt/functions $HOME/.zsh.prompts $fpath)
 promptinit
