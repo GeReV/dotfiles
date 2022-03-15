4d9c2b126e3fa2deb3c290eab99ca14831d486d6
Amir Grozki <amir.grozki@unityscm.com>
2022-03-15 11:31:53 +0200

    Correction.

--- run_install.sh
+++ run_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-e_header "Running '$0'"
-
 export SOURCE_DIR={{ .chezmoi.sourceDir }}
 
 source $SOURCE_DIR/source/00_common.sh
+
+e_header "Running '$0'"
 
 for file in ($SOURCE_DIR/scripts/*); do
     e_header "Sourcing $(basename "$file")"
