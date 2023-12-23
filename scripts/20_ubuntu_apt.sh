3e9417b36a4722b4928de51beaff11cc8924ed80
Amir Grozki <GeReV@users.noreply.github.com>
2023-12-23 02:02:50 +0200

    Don't ask before installing Docker from APT.

--- scripts/20_ubuntu_apt.sh
+++ scripts/20_ubuntu_apt.sh
@@ -211,7 +211,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Rehash
 hash -r
