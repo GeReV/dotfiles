4811aa3f6cbd4495024433c684a3ebea32855503
Amir Grozki <amirgrozki@gmail.com>
2024-08-03 16:35:48 +0300

    Install latest Ruby instead of 2.4.0.

--- scripts/40_devtools.sh
+++ scripts/40_devtools.sh
@@ -60,7 +60,7 @@ nvm install node
 
 # Install Ruby.
 if command -v rbenv &> /dev/null; then
-  versions=(2.4.0)
+  versions=(3.3.4)
 
   rubies=($(setdiff "${versions[*]}" "$(rbenv whence ruby)"))
   if (( ${#rubies[@]} > 0 )); then
